b92a84770a0f0d71653d5c45b417291f081fb6ea
rosdyana <rosdyana.kusuma@gmail.com>
2017-08-26 21:16:53 +0800

    fixed fields typo in user serializer

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from .serializer import *
+from api.serializer import *
 
 class UserViewSet(viewsets.ModelViewSet):
     """
